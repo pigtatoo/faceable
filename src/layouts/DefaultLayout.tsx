4ad6fce7abd4fa826000bddf2be4d66f82ee3fce
pigtatoo <140735238+pigtatoo@users.noreply.github.com>
2025-05-24 01:01:24 +0800

    og

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
+import WindowTitlebar from "../components/WindowTitlebar";
 import { AnimatePresence } from "framer-motion";
 import AnimatedRoute from "../components/AnimatedRoute";
-import Navbar from "../components/Navbar";
-
+import Navbarr from "../components/Navbar";
 
 export default function DefaultLayout() {
   return (
     <div className="relative flex flex-col justify-between">
       <div className="flex flex-col min-h-screen max-h-screen overflow-hidden">
         <div className="z-50">
-          <Navbar></Navbar>
+          <Navbarr></Navbarr>
         </div>
         <div className="relative flex-grow min-h-full h-full max-h-full overflow-auto bg-neutral-50 dark:bg-neutral-950">
           <AnimatePresence mode="wait">
